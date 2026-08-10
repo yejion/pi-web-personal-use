af69044c96ed8258fadd4535ff38bfeaa50c8a06
yejion <yejion@github.com>
2026-08-10 20:38:21 +0800

    fix: use os.EOL for newline in bump script (avoid CR/LF corruption)

--- electron/bump-version.cjs
+++ electron/bump-version.cjs
@@ -1,9 +1,11 @@
 const fs = require("fs");
+const os = require("os");
 const path = require("path");
 const pkgPath = path.join(__dirname, "..", "package.json");
 const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
 const parts = pkg.version.split("-")[0].split(".").map(Number);
 parts[2]++;
 pkg.version = parts.join(".");
-fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + String.fromCharCode(10));
+const output = JSON.stringify(pkg, null, 2) + os.EOL;
+fs.writeFileSync(pkgPath, output, "utf8");
 console.log(pkg.version);
