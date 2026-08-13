81542796ac71d53992b833abd659c02d8d6b9de2
laicong <1750142174@qq.com>
2026-08-13 12:50:12 +0800

    chore: remove stray debug files accidentally committed, ignore stackdumps

--- bin/_t3.js
+++
@@ -1,9 +0,0 @@
-console.log("line1");
-const fs = require("fs");
-console.log("fs ok");
-const path = require("path");
-console.log("path ok");
-const root = path.join(__dirname, "..");
-console.log("root:", root);
-const standalone = path.join(root, ".next", "standalone");
-console.log("exists:", fs.existsSync(path.join(standalone, "server.js")));
--- bin/_t4.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const root = path.join(__dirname, "..");
-const from = path.join(root, ".next", "static");
-const to = path.join(root, ".next", "standalone", ".next", "static");
-console.log("copying", from, "->", to);
-fs.rmSync(to, { recursive: true, force: true });
-fs.cpSync(from, to, { recursive: true });
-console.log("done");
